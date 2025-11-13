6679df1063572734cdd6ab2c505be0de0144ab96
Jason C. Klima <klimaj@uw.edu>
2025-11-12 20:45:55 -0800

    Clean up

--- pyrosettacluster_tests/uv/install_pyrosetta.py
+++ pyrosettacluster_tests/uv/install_pyrosetta.py
@@ -3,19 +3,20 @@ __author__ = "Jason C. Klima"
 
 import pyrosetta_installer
 
+kwargs = dict(
+    distributed=False,
+    serialization=True,
+    mirror=0,
+    type="Release",
+    use_setup_py_package=False,
+    skip_if_installed=False,
+    extras="",
+    silent=False,
+)
 
 try:
-    pyrosetta_installer.install_pyrosetta(
-        distributed=False,
-        serialization=True,
-        skip_if_installed=False,
-        mirror=0
-    )
+    pyrosetta_installer.install_pyrosetta(**kwargs)
 except Exception as ex:
     print(f"PyRosetta installation with 'mirror=0' failed: {ex}. Retrying with 'mirror=1'.")
-    pyrosetta_installer.install_pyrosetta(
-        distributed=False,
-        serialization=True,
-        skip_if_installed=False,
-        mirror=1
-    )
+    kwargs.update(mirror=1)
+    pyrosetta_installer.install_pyrosetta(**kwargs)
