59f8569213a9254f634e7e1950c05fc2ccc35e9d
Jason C. Klima <klimaj@uw.edu>
2026-01-24 00:18:59 -0800

    Add RMSD and total score tests to PyRosettaCluster TestAtomCoordinates test case (#10)

--- actions/pyrosettacluster/assert_coordinates.py
+++ actions/pyrosettacluster/assert_coordinates.py
@@ -2,6 +2,7 @@ __author__ = "Jason C. Klima"
 
 
 import argparse
+import pyrosetta
 import pyrosetta.distributed.io as io
 import unittest
 
@@ -21,11 +22,65 @@ class TestAtomCoordinates(unittest.TestCase):
                         float(getattr(res1.atom(atom).xyz(), axis)),
                         float(getattr(res2.atom(atom).xyz(), axis)),
                     )
-    
+
+    def assert_rmsd(self, pose1, pose2):
+        self.assertEqual(pose1.size(), pose2.size())
+        # Test RMSDs without superimposing
+        self.assertEqual(
+            pyrosetta.rosetta.core.scoring.all_atom_rmsd_nosuper(pose1, pose2),
+            0.0,
+        )
+        self.assertEqual(
+            pyrosetta.rosetta.core.scoring.all_scatom_rmsd_nosuper(pose1, pose2),
+            0.0,
+        )
+        for res in range(1, pose1.size() + 1):
+            v1 = pyrosetta.rosetta.utility.vector1_unsigned_long()
+            v1.append(res)
+            rmsd = pyrosetta.rosetta.core.scoring.all_atom_rmsd_nosuper(
+                pose1=pose1,
+                pose2=pose2,
+                pose1_residues=v1,
+                pose2_residues=v1,
+            )
+            self.assertEqual(rmsd, 0.0)
+        # Test RMSDs with superimposing
+        self.assertAlmostEqual(
+            pyrosetta.rosetta.core.scoring.all_atom_rmsd_incl_hydrogens(pose1, pose2),
+            0.0,
+            delta=1e-6,
+        )
+        self.assertAlmostEqual(
+            pyrosetta.rosetta.core.scoring.all_atom_rmsd(pose1, pose2),
+            0.0,
+            delta=1e-6,
+        )
+        self.assertAlmostEqual(
+            pyrosetta.rosetta.core.scoring.bb_rmsd(pose1, pose2),
+            0.0,
+            delta=1e-6,
+        )
+        self.assertAlmostEqual(
+            pyrosetta.rosetta.core.scoring.bb_rmsd_including_O(pose1, pose2),
+            0.0,
+            delta=1e-6,
+        )
+        self.assertAlmostEqual(
+            pyrosetta.rosetta.core.scoring.CA_rmsd(pose1, pose2),
+            0.0,
+            delta=1e-6,
+        )
+
+    def assert_total_score(self, pose1, pose2):
+        scorefxn = pyrosetta.get_score_function()
+        self.assertEqual(scorefxn(pose1), scorefxn(pose2))
+
     def test_coordinates(self):
         original_pose = io.pose_from_file(self.original_output_file).pose
         reproduce_pose = io.pose_from_file(self.reproduce_output_file).pose
         self.assert_atom_coordinates(original_pose, reproduce_pose)
+        self.assert_rmsd(original_pose, reproduce_pose)
+        self.assert_total_score(original_pose, reproduce_pose)
 
 
 if __name__ == "__main__":
